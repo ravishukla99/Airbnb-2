42bff328515e037c74a2b34d4d1ebfb1a9adf0b6
Shukla Ravi Rajendra <108168750+ravishukla99@users.noreply.github.com>
2023-06-22 15:31:03 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 // mpongoose compase
-mongoose.connect("mongodb://localhost:27017/AirbnbRegistation",{
+mongoose.connect("mongodb+srv://ravi1:ravi123@mongodblearn.yzso6tx.mongodb.net/?retryWrites=true&w=majority",{
+// mongoose.connect("mongodb://localhost:27017/AirbnbRegistation",{
 	useNewUrlParser:true,
 	useUnifiedTopology:true
 	
@@ -9,4 +10,5 @@ mongoose.connect("mongodb://localhost:27017/AirbnbRegistation",{
 }).catch((e)=>{
 	console.log("no connection")
 })
+
 
